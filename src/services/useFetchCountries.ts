a2f7d31ca890118fdefcdc3db70a256f4f6cbb3b
Dara BK <dara.neb@bi-kay.com>
2024-06-15 00:27:14 +0700

    Fetch country data

--- src/services/useFetchCountries.ts
+++ src/services/useFetchCountries.ts
@@ -1,3 +1,32 @@
+import type { CountryInfoType } from "@/types";
+import { ref } from "vue";
+
 export const useFetchCountries = () => {
-  return {};
+  const countries = ref<Array<CountryInfoType>>([]);
+  const error = ref<string>("");
+  const isFetching = ref(false);
+
+  const fetchCountryData = async () => {
+    isFetching.value = true;
+    try {
+      const response = await fetch(import.meta.env.VITE_COUNTRY_ENPOINT_ALL);
+      if (!response.ok) {
+        throw new Error("Failed to fetch country data!");
+      }
+      const responseData = (await response.json()) as Array<CountryInfoType>;
+      countries.value = responseData;
+    } catch (err) {
+      error.value = (err as Error).message;
+    } finally {
+      isFetching.value = false;
+    }
+  };
+
+  fetchCountryData();
+
+  return {
+    error,
+    countries,
+    isFetching,
+  };
 };
