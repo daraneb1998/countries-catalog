a13582f90388f9b1d73674d88cf1c489297f898c
Dara BK <dara.neb@bi-kay.com>
2024-06-14 19:11:40 +0700

    Feat: draft grid layout and mocked cards

---
+++ src/components/CountryItem.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = defineProps<{
+  person: {
+    name: string;
+    email: string;
+    image: string;
+  };
+}>();
+</script>
+
+<template>
+  <button class=" bg-red-500 hover:scale-105 transition-all rounded-s">
+    <img class="size-100" :src="props.person.image" alt="" />
+    <div>
+      <p class="font-bold truncate">{{ props.person.name }}</p>
+      <p class="text-sm text-gray-500">{{ props.person.email }}</p>
+    </div>
+  </button>
+</template>
