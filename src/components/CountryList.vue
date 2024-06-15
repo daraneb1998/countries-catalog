d68cd012d05d42825f7bdd9dc20476cd12efaa2d
Dara BK <dara.neb@bi-kay.com>
2024-06-15 10:52:01 +0700

    Adjust grid col

--- src/components/CountryList.vue
+++ src/components/CountryList.vue
@@ -10,7 +10,7 @@ const { countries } = useSearchStore();
     class="absolute top-[18vh] overflow-y-scroll w-screen h-[79vh] flex flex-row justify-center py-4 my-4"
   >
     <ul
-      class="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6 mx-8"
+      class="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6 mx-8"
     >
       <li v-for="country in countries" :key="country.flags.png">
         <CountryItem :country="country" :key="country.flags.png" />
