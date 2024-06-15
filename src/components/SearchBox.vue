06075b97cd6f178f5f2020eb27deab6c0971546a
Dara BK <dara.neb@bi-kay.com>
2024-06-15 10:11:16 +0700

    Removed unused icons

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -1,26 +1,14 @@
+<script setup lang="ts">
+import IconSearch from "@/components/icons/IconSearch.vue";
+</script>
+
 <template>
   <div class="w-1/2 absolute flex flex-row translate-x-1/2 top-[10%]">
     <div
       class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none"
       data-v-65e472f8=""
     >
-      <svg
-        aria-hidden="true"
-        class="w-5 h-5"
-        fill="none"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-        data-v-65e472f8=""
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-          data-v-65e472f8=""
-        ></path>
-      </svg>
+      <IconSearch />
     </div>
     <input
       id="default-search"
---
+++ src/components/icons/IconSearch.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg
+    aria-hidden="true"
+    class="w-5 h-5"
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    data-v-65e472f8=""
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+      data-v-65e472f8=""
+    ></path>
+  </svg>
+</template>
