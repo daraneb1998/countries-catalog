7482914d2855aecae8625871e2ce67d60a588b82
Dara BK <dara.neb@bi-kay.com>
2024-06-16 11:19:41 +0700

    Implemented search debounce

---
+++ src/components/Loader.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useSearchStore } from "@/stores/search";
+import Loading from "vue-loading-overlay";
+import "vue-loading-overlay/dist/css/index.css";
+
+const searchStore = useSearchStore();
+</script>
+
+<template>
+  <div class="vl-parent">
+    <Loading v-model="searchStore.isLoading" is-full-page="true" />
+  </div>
+</template>
--- src/stores/search.ts
+++ src/stores/search.ts
@@ -1,4 +1,4 @@
-import { computed, ref } from "vue";
+import { computed, reactive, ref } from "vue";
 import { defineStore } from "pinia";
 
 import { useFetchCountries } from "@/services/useFetchCountries";
@@ -13,6 +13,9 @@ export const useSearchStore = defineStore(
       defaultState.search.allCountries
     );
     const pageSize = ref<number>(defaultState.search.pageSize);
+    const isLoading = ref<boolean>(defaultState.search.isLoading);
+    const countryCaches = reactive(new Map<string, Array<CountryInfoType>>());
+
     const currentPageNumber = ref<number>(
       defaultState.search.currentPageNumber
     );
@@ -31,29 +34,37 @@ export const useSearchStore = defineStore(
       searchKeyword.value = keyword;
     }
 
-    function getCurrentCountries(sort: string) {
+    function getCachedCurrentCountries() {
       const startIndex = (currentPageNumber.value - 1) * pageSize.value;
-      return allCountries.value
-        .sort((a: CountryInfoType, b: CountryInfoType) => {
-          switch (sort) {
-            case SortType.ASCENDING:
-              return a.name.official.localeCompare(b.name.official);
-            case SortType.DESCENDING:
-              return b.name.official.localeCompare(a.name.official);
-            default:
-              return a.name.official.localeCompare(b.name.official);
-          }
-        })
-        .filter((country: CountryInfoType) => {
-          return country.name.official
-            .toLowerCase()
-            .includes(searchKeyword.value.toLowerCase());
-        })
-        .slice(startIndex, startIndex + pageSize.value);
+      return (keyword: string) => {
+        if (countryCaches.has(keyword)) {
+          return countryCaches.get(keyword);
+        }
+        const countryData = allCountries.value
+          .sort((a: CountryInfoType, b: CountryInfoType) => {
+            switch (sortType.value) {
+              case SortType.ASCENDING:
+                return a.name.official.localeCompare(b.name.official);
+              case SortType.DESCENDING:
+                return b.name.official.localeCompare(a.name.official);
+              default:
+                return a.name.official.localeCompare(b.name.official);
+            }
+          })
+          .filter((country: CountryInfoType) => {
+            return country.name.official
+              .toLowerCase()
+              .includes(keyword.toLowerCase());
+          })
+          .slice(startIndex, startIndex + pageSize.value);
+        countryCaches.set(keyword, countryData);
+        return countryData;
+      };
     }
 
     function updateSortType(type: SortType) {
       sortType.value = type;
+      isLoading.value = false;
     }
 
     function onGoToNextPage() {
@@ -68,7 +79,13 @@ export const useSearchStore = defineStore(
       }
     }
 
-    const countries = computed(() => getCurrentCountries(sortType.value));
+    function onLoading() {
+      isLoading.value = true;
+    }
+
+    const countries = computed(() =>
+      getCachedCurrentCountries()(searchKeyword.value)
+    );
     const totalPages = computed(() =>
       Math.ceil(allCountries.value.length / pageSize.value)
     );
@@ -77,12 +94,14 @@ export const useSearchStore = defineStore(
       error,
       sortType,
       pageSize,
+      isLoading,
       totalPages,
       allCountries,
       isFetching,
       currentPageNumber,
       countries,
       searchKeyword,
+      onLoading,
       updateSortType,
       setSearchKeyword,
       onGoToNextPage,
