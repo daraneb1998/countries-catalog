8ab8d1902914ff246f161c3e4bec4a8b4fe06528
Dara BK <dara.neb@bi-kay.com>
2024-06-17 18:58:47 +0700

    Update router import

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
 import App from "@/App.vue";
-import router from "./router";
+import router from "@/router";
 
 const app = createApp(App);
 const pinia = createPinia();
