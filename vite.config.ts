b889270183282c79a2a9a5b1afbecdc76a053582
Dara BK <dara.neb@bi-kay.com>
2024-06-17 18:38:09 +0700

    Add vue config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import VueDevTools from "vite-plugin-vue-devtools";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), VueDevTools()],
-  base: "/",
+  base: "/countries-catalog/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
